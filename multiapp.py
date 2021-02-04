25933144bd3a83acbd6f7bfe76a5b7fdca434792
reejung.kim <reejung.kim@gmail.com>
2021-02-04 23:09:25 +0000

    First commit

---
+++ multiapp.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 23:06:59 2021
+
+@author: reejungkim
+"""
+
+"""Frameworks for running multiple Streamlit applications as a single app.
+"""
+import streamlit as st
+
+class MultiApp:
+    """Framework for combining multiple streamlit applications.
+    Usage:
+        def foo():
+            st.title("Hello Foo")
+        def bar():
+            st.title("Hello Bar")
+        app = MultiApp()
+        app.add_app("Foo", foo)
+        app.add_app("Bar", bar)
+        app.run()
+    It is also possible keep each application in a separate file.
+        import foo
+        import bar
+        app = MultiApp()
+        app.add_app("Foo", foo.app)
+        app.add_app("Bar", bar.app)
+        app.run()
+    """
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        """Adds a new application.
+        Parameters
+        ----------
+        func:
+            the python function to render this app.
+        title:
+            title of the app. Appears in the dropdown in the sidebar.
+        """
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run(self):
+        # app = st.sidebar.radio(
+        app = st.selectbox(
+            'Navigation',
+            self.apps,
+            format_func=lambda app: app['title'])
+
+        app['function']()
