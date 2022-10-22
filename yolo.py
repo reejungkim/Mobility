c7011aa3ebd238ce6458197d0fbb46e32b55b446
reejung.kim <reejung.kim@gmail.com>
2022-10-22 15:00:36 +0900

    Add darknet modified

--- yolo.py
+++ yolo.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import pandas as pd
 import cv2
@@ -19,54 +18,145 @@ import time
 import sys
 
 
-def main():
-    new_title = '<p style="font-size: 42px;">Welcome to my Object Detection App!</p>'
-    read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
+def object_detection_video():
+    #object_detection_video.has_beenCalled = True
+    # pass
+    CONFIDENCE = 0.5
+    SCORE_THRESHOLD = 0.5
+    IOU_THRESHOLD = 0.5
+    config_path = r'config_n_weights\yolov3.cfg'
+    weights_path = r'config_n_weights\yolov3.weights'
+    font_scale = 1
+    thickness = 1
+    url = "https://raw.githubusercontent.com/zhoroh/ObjectDetection/master/labels/coconames.txt"
+    f = urllib.request.urlopen(url)
+    labels = [line.decode('utf-8').strip() for line in f]
+    #f = open(r'C:\Users\Olazaah\Downloads\stream\labels\coconames.txt','r')
+    #lines = f.readlines()
+    #labels = [line.strip() for line in lines]
+    colors = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
 
-    read_me = st.markdown("""
-    This project was built using Streamlit and OpenCV 
-    to demonstrate YOLO Object detection in both videos(pre-recorded)
-    and images.
-    
-    
-    This YOLO object Detection project can detect 80 objects(i.e classes)
-    in either a video or image. The full list of the classes can be found 
-    [here](https://github.com/KaranJagtiani/YOLO-Coco-Dataset-Custom-Classes-Extractor/blob/main/classes.txt)"""
-                          )
-    st.sidebar.title("Select Activity")
-    choice = st.sidebar.selectbox(
-        "MODE", ("About", "Object Detection(Image)", "Object Detection(Video)"))
-    # ["Show Instruction","Landmark identification","Show the #source code", "About"]
+    net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
 
-    if choice == "Object Detection(Image)":
-        #st.subheader("Object Detection")
-        read_me_0.empty()
-        read_me.empty()
-        #st.title('Object Detection')
-        object_detection_image()
-    elif choice == "Object Detection(Video)":
-        read_me_0.empty()
-        read_me.empty()
-        #object_detection_video.has_beenCalled = False
-        object_detection_video()
-        # if object_detection_video.has_beenCalled:
-        try:
+    ln = net.getLayerNames()
+    ln = [ln[i - 1] for i in net.getUnconnectedOutLayers()]
+    st.title("Object Detection for Videos")
+    st.subheader("""
+    This object detection project takes in a video and outputs the video with bounding boxes created around the objects in the video 
+    """
+                 )
+    uploaded_video = st.file_uploader(
+        "Upload Video", type=['mp4', 'mpeg', 'mov'])
+    if uploaded_video != None:
 
-            clip = moviepy.VideoFileClip('detected_video.mp4')
-            clip.write_videofile("myvideo.mp4")
-            st_video = open('myvideo.mp4', 'rb')
-            video_bytes = st_video.read()
-            st.video(video_bytes)
-            st.write("Detected Video")
-        except OSError:
-            ''
+        vid = uploaded_video.name
+        with open(vid, mode='wb') as f:
+            f.write(uploaded_video.read())  # save video to disk
 
-    elif choice == "About":
-        print()
+        st_video = open(vid, 'rb')
+        video_bytes = st_video.read()
+        st.video(video_bytes)
+        st.write("Uploaded Video")
+        #video_file = 'street.mp4'
+        cap = cv2.VideoCapture(vid)
+        _, image = cap.read()
+        h, w = image.shape[:2]
+        # out = cv2.VideoWriter(output_name, cv2.VideoWriter_fourcc#(*'avc3'), fps, insize)
 
+        fourcc = cv2.VideoWriter_fourcc(*'mpv4')
+        out = cv2.VideoWriter("detected_video.mp4", fourcc, 20.0, (w, h))
+        count = 0
+        while True:
+            _, image = cap.read()
+            if _ != False:
+                h, w = image.shape[:2]
+                blob = cv2.dnn.blobFromImage(
+                    image, 1/255.0, (416, 416), swapRB=True, crop=False)
+                net.setInput(blob)
+                start = time.perf_counter()
+                layer_outputs = net.forward(ln)
+                time_took = time.perf_counter() - start
+                count += 1
+                print(f"Time took: {count}", time_took)
+                boxes, confidences, class_ids = [], [], []
 
-if __name__ == '__main__':
-    main()
+                # loop over each of the layer outputs
+                for output in layer_outputs:
+                    # loop over each of the object detections
+                    for detection in output:
+                        # extract the class id (label) and confidence (as a probability) of
+                        # the current object detection
+                        scores = detection[5:]
+                        class_id = np.argmax(scores)
+                        confidence = scores[class_id]
+                        # discard weak predictions by ensuring the detected
+                        # probability is greater than the minimum probability
+                        if confidence > CONFIDENCE:
+                            # scale the bounding box coordinates back relative to the
+                            # size of the image, keeping in mind that YOLO actually
+                            # returns the center (x, y)-coordinates of the bounding
+                            # box followed by the boxes' width and height
+                            box = detection[:4] * np.array([w, h, w, h])
+                            (centerX, centerY, width, height) = box.astype("int")
+
+                            # use the center (x, y)-coordinates to derive the top and
+                            # and left corner of the bounding box
+                            x = int(centerX - (width / 2))
+                            y = int(centerY - (height / 2))
+
+                            # update our list of bounding box coordinates, confidences,
+                            # and class IDs
+                            boxes.append([x, y, int(width), int(height)])
+                            confidences.append(float(confidence))
+                            class_ids.append(class_id)
+
+                # perform the non maximum suppression given the scores defined before
+                idxs = cv2.dnn.NMSBoxes(
+                    boxes, confidences, SCORE_THRESHOLD, IOU_THRESHOLD)
+
+                font_scale = 0.6
+                thickness = 1
+
+                # ensure at least one detection exists
+                if len(idxs) > 0:
+                    # loop over the indexes we are keeping
+                    for i in idxs.flatten():
+                        # extract the bounding box coordinates
+                        x, y = boxes[i][0], boxes[i][1]
+                        w, h = boxes[i][2], boxes[i][3]
+                        # draw a bounding box rectangle and label on the image
+                        color = [int(c) for c in colors[class_ids[i]]]
+                        cv2.rectangle(image, (x, y), (x + w, y + h),
+                                      color=color, thickness=thickness)
+                        text = f"{labels[class_ids[i]]}: {confidences[i]:.2f}"
+                        # calculate text width & height to draw the transparent boxes as background of the text
+                        (text_width, text_height) = cv2.getTextSize(
+                            text, cv2.FONT_HERSHEY_SIMPLEX, fontScale=font_scale, thickness=thickness)[0]
+                        text_offset_x = x
+                        text_offset_y = y - 5
+                        box_coords = ((text_offset_x, text_offset_y),
+                                      (text_offset_x + text_width + 2, text_offset_y - text_height))
+                        overlay = image.copy()
+                        cv2.rectangle(
+                            overlay, box_coords[0], box_coords[1], color=color, thickness=cv2.FILLED)
+                        # add opacity (transparency to the box)
+                        image = cv2.addWeighted(overlay, 0.6, image, 0.4, 0)
+                        # now put the text (label: confidence %)
+                        cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                                    fontScale=font_scale, color=(0, 0, 0), thickness=thickness)
+
+                out.write(image)
+                cv2.imshow("image", image)
+
+                if ord("q") == cv2.waitKey(1):
+                    break
+            else:
+                break
+
+        # return "detected_video.mp4"
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 def object_detection_image():
@@ -155,3 +245,53 @@ def object_detection_image():
 
         cv2.destroyAllWindows()
         my_bar.progress(100)
+
+
+def main():
+    new_title = '<p style="font-size: 42px;">Welcome to my Object Detection App!</p>'
+    read_me_0 = st.markdown(new_title, unsafe_allow_html=True)
+
+    read_me = st.markdown("""
+    This project was built using Streamlit and OpenCV 
+    to demonstrate YOLO Object detection in both videos(pre-recorded)
+    and images.
+    
+    
+    This YOLO object Detection project can detect 80 objects(i.e classes)
+    in either a video or image. The full list of the classes can be found 
+    [here](https://github.com/KaranJagtiani/YOLO-Coco-Dataset-Custom-Classes-Extractor/blob/main/classes.txt)"""
+                          )
+    st.sidebar.title("Select Activity")
+    choice = st.sidebar.selectbox(
+        "MODE", ("About", "Object Detection(Image)", "Object Detection(Video)"))
+    # ["Show Instruction","Landmark identification","Show the #source code", "About"]
+
+    if choice == "Object Detection(Image)":
+        #st.subheader("Object Detection")
+        read_me_0.empty()
+        read_me.empty()
+        #st.title('Object Detection')
+        object_detection_image()
+    elif choice == "Object Detection(Video)":
+        read_me_0.empty()
+        read_me.empty()
+        #object_detection_video.has_beenCalled = False
+        object_detection_video()
+        # if object_detection_video.has_beenCalled:
+        try:
+
+            clip = moviepy.VideoFileClip('detected_video.mp4')
+            clip.write_videofile("myvideo.mp4")
+            st_video = open('myvideo.mp4', 'rb')
+            video_bytes = st_video.read()
+            st.video(video_bytes)
+            st.write("Detected Video")
+        except OSError:
+            ''
+
+    elif choice == "About":
+        print()
+
+
+if __name__ == '__main__':
+    main()
